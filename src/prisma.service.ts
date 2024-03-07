26a6c831abeb3baa73ae95f52d9db7c0334a6724
Elias Norrby <elias.norrby@gmail.com>
2024-03-07 15:02:43 +0100

    log request id in '$on' handler

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,8 +1,24 @@
-import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
+import { Injectable, OnModuleInit, INestApplication, Logger } from '@nestjs/common'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { ClsService } from 'nestjs-cls'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query'>
+  implements OnModuleInit
+{
+  constructor(private cls: ClsService) {
+    super({
+      log: [{ emit: 'event', level: 'query' }]
+    })
+
+    this.$on('query', (e) => {
+      const requestId = this.cls.getId()
+      this.logger.log(`handler, requestId: '${requestId}'`)
+    })
+  }
+
+  private readonly logger = new Logger(PrismaService.name)
   async onModuleInit() {
     // Note: this is optional
     await this.$connect()
